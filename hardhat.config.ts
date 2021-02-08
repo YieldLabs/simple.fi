fb6a48bd421457a3b24af6b62dcf3cd8ee70c0a1
Siarhei Melnik <siarhei.m.a@gmail.com>
2021-02-09 00:33:23 +0300

    update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,5 @@
 import "@nomiclabs/hardhat-waffle";
+import "@openzeppelin/hardhat-upgrades";
 import "@nomiclabs/hardhat-ethers";
 import * as dotenv from 'dotenv';
 import { HardhatUserConfig } from "hardhat/config";
