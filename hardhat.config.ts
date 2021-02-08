db7e521e4cdcb4ec3cd890ad62994703769560d1
Siarhei Melnik <siarhei.m.a@gmail.com>
2021-02-08 23:55:13 +0300

    update config

---
+++ hardhat.config.ts
@@ -0,0 +1,32 @@
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+import * as dotenv from 'dotenv';
+import { HardhatUserConfig } from "hardhat/config";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+        blockNumber: 11095000
+      }
+    }
+  },
+  solidity: {
+    version: "0.8.1",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
+
+export default config;
