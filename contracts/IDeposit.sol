c6c62804b0ca36b7c4732675bf330db6edacc2bc
Siarhei Melnik <siarhei.m.a@gmail.com>
2021-02-10 22:47:53 +0300

    add deposit interface

--- contracts/IDeposit.sol
+++ contracts/IDeposit.sol
@@ -2,4 +2,23 @@ pragma solidity ^0.8.1;
 
 interface IDeposit {
 
+    function deposit(
+        address from,
+        address token,
+        uint96 amount
+    ) external payable returns (uint96 received);
+
+    function withdraw(
+        address from,
+        address to,
+        address token,
+        uint amount
+    ) external payable;
+
+    function transfer(
+        address from,
+        address to,
+        address token,
+        uint amount
+    ) external payable;
 }
