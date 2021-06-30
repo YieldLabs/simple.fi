6286b18c946590873c8c8725b4f02a58b9cd74c2
Siarhei Melnik <siarhei.m.a@gmail.com>
2021-06-30 14:44:49 +0300

    init

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,33 +1,46 @@
+import { HardhatUserConfig } from "hardhat/types";
+import * as dotenv from "dotenv";
+
 import "@nomiclabs/hardhat-waffle";
 import "@openzeppelin/hardhat-upgrades";
 import "@nomiclabs/hardhat-ethers";
-import * as dotenv from 'dotenv';
-import { HardhatUserConfig } from "hardhat/config";
+import "hardhat-typechain";
 
 dotenv.config();
 
 const config: HardhatUserConfig = {
-  defaultNetwork: "hardhat",
+  defaultNetwork: "matic",
   networks: {
     localhost: {
-      url: "http://127.0.0.1:8545"
+      url: "http://127.0.0.1:8545",
+    },
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [PRIVATE_KEY],
+    },
+    matic: {
+      url: "https://rpc-mainnet.matic.network",
     },
     hardhat: {
       forking: {
         url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
-        blockNumber: 11095000
-      }
-    }
+        blockNumber: 11095000,
+      },
+    },
+  },
+  typechain: {
+    outDir: "types",
+    target: "ethers-v5",
   },
   solidity: {
-    version: "0.8.1",
+    version: "0.8.6",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
-  }
+        runs: 200,
+      },
+    },
+  },
 };
 
 export default config;
